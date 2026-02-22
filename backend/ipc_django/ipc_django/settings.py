47638eb2d343f17ecd281bce4edc0c6a684e2123
suhas <suhasyanamadala@gmail.com>
2026-02-22 17:39:50 +0530

    change settings.py

--- backend/ipc_django/ipc_django/settings.py
+++ backend/ipc_django/ipc_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_vwefm%0lem4^#uw5enlo$q48k7&@t2eu!9%ows-#ootqgx3f+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
